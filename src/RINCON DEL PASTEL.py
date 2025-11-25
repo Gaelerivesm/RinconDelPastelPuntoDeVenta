563e9b7789644385bcf8a9834ec196618cd57043
0-fernanda <AL07144258@tecmilenio.mx>
2025-11-24 22:10:03 -0600

    Primera parte diccionario

---
+++ src/RINCON DEL PASTEL.py
@@ -0,0 +1,13 @@
+# ======= INVENTARIO =======
+ingredientes = {
+    "harina": {"cantidad": 10, "costo": 12},
+    "azucar": {"cantidad": 8, "costo": 10},
+    "huevo": {"cantidad": 30, "costo": 2}
+}
+
+pasteles = {
+    "Chocolate": {
+        "ingredientes": {"harina": 2, "azucar": 1, "huevo": 3},
+        "ganancia": 0.30
+    }
+}
