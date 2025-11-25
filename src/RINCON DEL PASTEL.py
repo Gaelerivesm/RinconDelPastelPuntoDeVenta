4aea1d82695052de560c7c14edc33066399c5c8f
0-fernanda <AL07144258@tecmilenio.mx>
2025-11-24 22:12:21 -0600

    Agregue las funciones eliminar, agregar, modificar

--- src/RINCON DEL PASTEL.py
+++ src/RINCON DEL PASTEL.py
@@ -11,3 +11,55 @@ pasteles = {
         "ganancia": 0.30
     }
 }
+# ======= PEDIDOS =======
+pedidos = []
+
+# ====== FUNCIONES EN INGREDIENTES ======
+def agregar_ingrediente(nombre, cantidad, costo):
+    if nombre in ingredientes:
+        print("El ingrediente ya existe.")
+        return
+    ingredientes[nombre] = {"cantidad": cantidad, "costo": costo}
+    print("Ingrediente agregado.")
+
+def modificar_ingrediente(nombre, nueva_cantidad=None, nuevo_costo=None):
+    if nombre not in ingredientes:
+        print("El ingrediente no existe.")
+        return
+    if nueva_cantidad is not None:
+        ingredientes[nombre]["cantidad"] = nueva_cantidad
+    if nuevo_costo is not None:
+        ingredientes[nombre]["costo"] = nuevo_costo
+    print("Ingrediente modificado.")
+
+def eliminar_ingrediente(nombre):
+    if nombre not in ingredientes:
+        print("El ingrediente no existe.")
+        return
+    del ingredientes[nombre]
+    print("Ingrediente eliminado.")
+
+# ======= PASTELES =======
+def agregar_pastel(nombre, ingredientes_usados, ganancia):
+    if nombre in pasteles:
+        print("Ese pastel ya existe.")
+        return
+    pasteles[nombre] = {"ingredientes": ingredientes_usados, "ganancia": ganancia}
+    print("Pastel agregado.")
+
+def modificar_pastel(nombre, nuevos_ingredientes=None, nueva_ganancia=None):
+    if nombre not in pasteles:
+        print("Ese pastel no existe.")
+        return
+    if nuevos_ingredientes is not None:
+        pasteles[nombre]["ingredientes"] = nuevos_ingredientes
+    if nueva_ganancia is not None:
+        pasteles[nombre]["ganancia"] = nueva_ganancia
+    print("Pastel modificado.")
+
+def eliminar_pastel(nombre):
+    if nombre not in pasteles:
+        print("Ese pastel no existe.")
+        return
+    del pasteles[nombre]
+    print("Pastel eliminado.")
